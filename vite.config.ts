6ec9bcd1272cc0cde3aa08b77e0546548ffd17b9
Luan Eduardo <luan1346@gmail.com>
2023-01-02 21:59:49 -0300

    Add PWA configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,64 @@
 /// <reference types="vitest" />
 
 import path from 'path'
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Worfind',
+        short_name: 'Worfind',
+        description: 'Performs analysis and manipulations on strings',
+        theme_color: '#ffffff',
+        lang: 'pt-BR',
+        icons: [
+          {
+            src: '/favicons/favicon-16x16.png',
+            sizes: '16x16',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+          {
+            src: '/favicons/favicon-32x32.png',
+            sizes: '32x32',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+          {
+            src: '/favicons/favicon-96x96.png',
+            sizes: '96x96',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+          {
+            src: '/favicons/favicon-144x144.png',
+            sizes: '144x144',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+          {
+            src: '/favicons/favicon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+          {
+            src: '/favicons/favicon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       src: path.resolve(__dirname, './src'),
